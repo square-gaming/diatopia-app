fef313b668b8a0a66b3f7872e1659c2135bb2a21
Joseph <josephchen.jojo@gmail.com>
2021-03-09 14:56:57 +0800

    Add a goat mob

--- src/models/entity/index.ts
+++ src/models/entity/index.ts
@@ -1,9 +1,11 @@
 import Cow from "./mobs/Cow";
+import Goat from "./mobs/Goat";
 
 const constructors: {
     [entitiesType: string]: any;
 } = {
-    'Cow': Cow
+    'Cow': Cow, 
+    'Goat': Goat
 };
 
 export default constructors;

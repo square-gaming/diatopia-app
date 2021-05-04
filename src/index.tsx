f93c0b537ca8443c136178d777ca3a0631b9e0f0
Asriel Yu <luffy84217@gmail.com>
2021-05-04 12:10:36 +0800

    fix(core): remove unused snip

--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import Entry from './containers/Entry';
 import reportWebVitals from './reportWebVitals';
-import { createIntlProvider } from './context/intl';
-import { createStoreProvider } from './context/store';
-import reducer from './reducers';
-import storeState from './config/storeState';
-import langs from './translations';
 import './index.css';
 
-const IntlProvider = createIntlProvider(langs.en);
-const StoreProvider = createStoreProvider(reducer, storeState);
 const MOUNT_NODE = document.getElementById('dr') as HTMLDivElement;
 
 render(
-    <StoreProvider>
-        <IntlProvider>
-            <Entry />
-        </IntlProvider>
-    </StoreProvider>,
+    <Entry />,
     MOUNT_NODE
 );
 
